bd33e6f65ac3d57b7afb1cc873f4cabfc8b561ff
QuacLam <lam.22042003@gmail.com>
2024-11-28 12:27:34 +0700

    debug..

--- Assets/Script/GamePlayManager.cs
+++ Assets/Script/GamePlayManager.cs
@@ -14,7 +14,7 @@ public class GamePlayManager : MonoBehaviour
         currentLevel.SetActive(true);
         GameObject currentCar = Instantiate(Cars[PlayerPrefs.GetInt("CarNumber")]); 
         currentCar.transform.position = currentLevel.transform.GetChild(0).transform.position;
-         
+        //Debug.. 
     }
 
     // Update is called once per frame
